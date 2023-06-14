ec3a46f316e645aafaaed71c775255df6a058f9c
randelyuan <291885827@qq.com>
2023-06-14 22:47:17 +0800

    修改侧边导航栏

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -3,18 +3,6 @@ import { sidebar } from "vuepress-theme-hope";
 export default sidebar({
   "/": [
     {
-      text: '主页',
-      icon: 'home',
-      prefix: '/',
-      link: '/',
-    },
-    {
-      text: '简介',
-      icon: 'code',
-      prefix: 'guide/',
-      link: 'guide/',
-    },
-    {
       text: '开发工具',
       icon: '/assets/icon/tools-hardware.png',
       prefix: 'tools/',

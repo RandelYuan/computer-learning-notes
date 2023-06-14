216179987da00efa330090b927bb9b0f0347149f
randelyuan <291885827@qq.com>
2023-06-14 22:55:12 +0800

    修改头部导航条中GitHub的地址

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,13 +2,16 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 import { searchProPlugin } from "vuepress-plugin-search-pro";
 import { containerPlugin } from '@vuepress/plugin-container'
+import {hopeTheme} from "vuepress-theme-hope";
 
 export default defineUserConfig({
   base: "/computer-learning-notes/",
   lang: "zh-CN",
   title: "Randel's Computer Learning Notes",
   description: "Randel's Computer Learning Notes",
-  theme,
+  theme: hopeTheme({
+    repo: 'https://github.com/RandelYuan/computer-learning-notes/',
+  }),
   plugins: [
     searchProPlugin({
       // 索引全部内容
